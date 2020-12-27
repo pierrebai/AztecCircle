c6f4bb7e561c61c6e7de82bdf3df4b2e52b55a86
Pierre Baillargeon <pierrebai@hotmail.com>
2020-12-27 12:36:26 -0500

    Simplified main.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from aztec_circle import aztec
 from draw_ascii import draw_ascii_reactor
+from reactor import reactor
 from repeatable_random import repeatable_random
 
 seed = 7
-az = aztec(0, repeatable_random(seed), draw_ascii_reactor())
-for i in range(0, 6):
-    az.grow()
-    print('')
-    print('')
-
+az = aztec(10, repeatable_random(seed), draw_ascii_reactor())
