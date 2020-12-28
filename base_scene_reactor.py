133ef529843890715c7be9fa662bcbd499dace0d
Pierre Baillargeon <pierrebai@hotmail.com>
2020-12-27 21:29:20 -0500

    Added tile borders.

--- base_scene_reactor.py
+++ base_scene_reactor.py
@@ -1,8 +1,8 @@
 from reactor import reactor
 
-from PyQt5.QtGui import QBrush, QColor, QPen
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsItem, QGraphicsRectItem
-from PyQt5.QtCore import QMarginsF, QRectF, Qt
+from PyQt5.QtGui import QBrush, QColor, QPen, QPolygonF
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsItem, QGraphicsRectItem, QGraphicsPolygonItem, QGraphicsLineItem
+from PyQt5.QtCore import QMarginsF, QRectF, QPointF, QLineF, Qt
 
 
 tile_size = 10
@@ -11,19 +11,37 @@ class base_scene_reactor(reactor):
     tile_pens = [ [QPen(tile_colors[0].darker(120)), QPen(tile_colors[1].darker(120))], [QPen(tile_colors[2].darker(120)), QPen(tile_colors[3].darker(120))] ]
     tile_brushes = [ [QBrush(tile_colors[0]), QBrush(tile_colors[1])], [QBrush(tile_colors[2]), QBrush(tile_colors[3])] ]
 
+    black_pen = QPen(QColor(0, 0, 0))
+
+    points = [QPointF(0, 0), QPointF(tile_size, 0), QPointF(tile_size, tile_size), QPointF(0, tile_size), QPointF(0, 0), QPointF(tile_size, 0), QPointF(tile_size, tile_size), QPointF(0, tile_size)]
+    ranges = [[(3, 6), (1, 4)], [(2, 5), (0, 3)]]
+
     def __init__(self, scene: QGraphicsScene, view: QGraphicsView):
         self.scene = scene
         self.view = view
-        self.tile_size = 10
 
     def create_scene_tile(self, pos: tuple, tile) -> QGraphicsItem:
-        ts = tile_size
-        item = QGraphicsRectItem(0, 0, ts, ts)
-        item.setPos(*self.pos_to_scene(pos))
+        items = []
+
+        scene_pos = self.pos_to_scene(pos)
+
+        item = QGraphicsRectItem(0, 0, tile_size, tile_size)
+        item.setPos(*scene_pos)
         item.setBrush(base_scene_reactor.tile_to_brush(tile))
         item.setPen(base_scene_reactor.tile_to_pen(tile))
-        self.scene.addItem(item)
-        return item
+        items.append(item)
+
+        start, end = base_scene_reactor.ranges[tile.is_horizontal][tile.is_high_part]
+        for i in range(start, end):
+            item = QGraphicsLineItem(QLineF(base_scene_reactor.points[i], base_scene_reactor.points[i+1]))
+            item.setPos(*scene_pos)
+            item.setPen(base_scene_reactor.black_pen)
+            items.append(item)
+
+        for item in items:
+            self.scene.addItem(item)
+
+        return items
 
     def adjust_view_to_fit(self):
         self.scene.update()
@@ -33,8 +51,7 @@ class base_scene_reactor(reactor):
             self.view.fitInView(QRectF(0, 0, 200, 200).united(self.scene.sceneRect().marginsAdded(QMarginsF(100, 100, 100, 100))), Qt.KeepAspectRatio)
 
     def pos_to_scene(self, pos: tuple) -> tuple:
-        ts = tile_size
-        return (pos[0] * ts, pos[1] * ts)
+        return (pos[0] * tile_size, pos[1] * tile_size)
 
     @staticmethod
     def tile_to_brush(tile) -> QBrush:
--- step_scene_reactor.py
+++ step_scene_reactor.py
@@ -19,18 +19,20 @@ class step_scene_reactor(base_scene_reactor):
         self.adjust_view_to_fit()
 
     def collision(self, az, pos1, pos2):
-        self.scene.removeItem(self.items[pos1])
-        self.scene.removeItem(self.items[pos2])
+        for item in self.items[pos1]:
+            self.scene.removeItem(item)
+        for item in self.items[pos2]:
+            self.scene.removeItem(item)
 
     def move(self, az, pos1, pos2):
-        item = self.items[pos1]
-        item.setPos(*self.pos_to_scene(pos2))
-        #item.setRect(*self.pos_to_scene(pos2), self.tile_size, self.tile_size)
-        self.new_items[pos2] = item
+        items = self.items[pos1]
+        for item in items:
+            item.setPos(*self.pos_to_scene(pos2))
+        self.new_items[pos2] = items
 
     def fill(self, az, pos, tile):
-        item = self.create_scene_tile(pos, tile)
-        self.new_items[pos] = item
+        items = self.create_scene_tile(pos, tile)
+        self.new_items[pos] = items
 
     def end_grow(self, az):
         self.items = self.new_items
